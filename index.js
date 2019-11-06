ace49406ecc92821199fa0f84ed0da102598eeb0
Ammar Dwi Anwari <weissman1505199@gmail.com>
2019-11-07 05:59:13 +0700

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var socket =  require('socket.io');
+
+// App Setup
+var app = express();
+var port = process.env.PORT || 3000;
+
+var path = require('path');
+var server = require('http').createServer(app);
+server.listen(port,function(){
+    console.log('listening request on port ' + port);
+});
+
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Socket setup
+var io = socket(server);
+// var io = require('../..')(server);
+
+io.on('connection', function(socket){
+    console.log("made socket connection with " + socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    })
+});
