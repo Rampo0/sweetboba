680bca4c273ef2e2a9f219ee7839c20ff7318679
Claus Peter <34019306+clausaxon@users.noreply.github.com>
2019-12-14 14:13:55 +0700

    update
    update

--- public/js/SceneManager.js
+++ public/js/SceneManager.js
@@ -2,7 +2,7 @@ function SceneManager(canvas) {
 
     //Init
     const clock = new THREE.Clock();
-    
+
     const screenDimensions = {
         width: canvas.width,
         height: canvas.height
@@ -13,67 +13,70 @@ function SceneManager(canvas) {
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.right = '0px';
     stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-
-    // var rendererStats	= new THREEx.RendererStats()
-    // rendererStats.domElement.style.position	= 'absolute'
-    // rendererStats.domElement.style.left	= '0px'
-    // rendererStats.domElement.style.bottom	= '0px'
-    // document.body.appendChild( rendererStats.domElement )
+    // document.body.appendChild(stats.domElement);
+    $('.route-view').append(stats.domElement);
 
     const groundMaterial = new CANNON.Material("groundMaterial");
-    const slipperyMaterial = new CANNON.Material("slipperyMaterial");        
+    const slipperyMaterial = new CANNON.Material("slipperyMaterial");
     var player;
+    const ui = new GameUI();
+
+    // setup game manager
+    const gm = new GameManager();
+
+    function scoreCounting() {
+        gm.score++;
+    }
+    setInterval(scoreCounting, 1000);
 
     const world = buildWorld();
     const scene = buildScene();
     const renderer = buildRender(screenDimensions);
     const camera = buildCamera(screenDimensions);
-    // const audio = buildAudio(camera);
-    const sceneObjects = createSceneObjects(scene);
-    var timeStep = 1 / 60;
     
-    // var axesHelper = new THREE.AxesHelper(500);
-    // scene.add( axesHelper );
-
-    // var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    
-    buildPhysicsContactMaterial();
-  
-    //Build Audio
+    // Build Audio
+    const audioManager = new AudioManager(camera);
 
     // create an AudioListener and add it to the camera
     var listener = new THREE.AudioListener();
-    camera.add( listener );
+    camera.add(listener);
 
     // create a global audio source
-    var sound = new THREE.Audio( listener );
+    var sound = new THREE.Audio(listener);
 
     // load a sound and set it as the Audio object's buffer
-    var audioLoader = new THREE.AudioLoader();
+    const audioLoader = new THREE.AudioLoader();
 
-    audioLoader.load( '/assets/sounds/bg.ogg', function( buffer ) {
-        sound.setBuffer( buffer );
-        sound.setLoop( true );
-        sound.setVolume( 1 );
+    audioLoader.load('/assets/sounds/bg.ogg', function (buffer) {
+        if(sound.source){
+            sound.stop();
+        }
+        sound.setBuffer(buffer);
+        sound.setLoop(true);
+        sound.setVolume(1);
         sound.play();
     });
+
+    const sceneObjects = createSceneObjects(scene);
+    var timeStep = 1 / 60;
+
+    buildPhysicsContactMaterial();
 
     // Sensor Setup
 
     var sensorY;
     var socket = io()
-    
-    socket.on('sensorY',function(data){
+
+    socket.on('sensorY', function (data) {
         sensorY = data;
     })
 
-    socket.on('sensorZ',function(data){
+    socket.on('sensorZ', function (data) {
         // console.log(data);
     })
 
     // End Init
-    
+
     function buildScene() {
         const scene = new THREE.Scene();
         scene.background = new THREE.Color("#ff9ae9");
@@ -90,7 +93,7 @@ function SceneManager(canvas) {
 
         return world;
     }
-    
+
     function buildRender({
         width,
         height
@@ -132,21 +135,17 @@ function SceneManager(canvas) {
 
         const PointLight = new GeneralLights(scene);
         const Ambientlight = new AmbientLight(scene);
-        // const skybox = new Skybox(scene);
-        // var text = new MyText(scene);
-        player = new Cone(scene, world, slipperyMaterial , {
+        player = new Cone(scene, world, slipperyMaterial, {
             scaleX: 10,
             scaleY: 30,
             radSegments: 32
-        });
-        var plane = new Plane(scene, world , groundMaterial);
-        // var obstacle = new Box(scene, world , groundMaterial , 0);
-        
+        }, audioManager);
+        var plane = new Plane(scene, world, groundMaterial);
+
         // Assign Dynamic Object
 
         const sceneObjects = [
-            // cone,
-            // obstacle,
+
         ];
 
         return sceneObjects;
@@ -154,10 +153,11 @@ function SceneManager(canvas) {
 
     // seconds counting
     var seconds = 0.0;
-    function Counting(){
+
+    function Counting() {
         seconds += 0.01;
     }
-    var cancel = setInterval(Counting , 10);
+    var cancel = setInterval(Counting, 10);
     // end second counting
 
     this.update = function () {
@@ -167,11 +167,11 @@ function SceneManager(canvas) {
         const elapsedTime = clock.getElapsedTime();
         world.step(timeStep);
 
-        if(seconds >= 1){
+        if (seconds >= 1) {
             // todo every 5 seconds
             seconds = 0.0;
             // Spawn obstacles
-            sceneObjects.push(new Box(scene, world , groundMaterial , GetRandomInt(-185 , 185)));
+            sceneObjects.push(new Box(scene, world, groundMaterial, GetRandomInt(-185, 185)));
         }
 
         for (let i = 0; i < sceneObjects.length; i++) {
@@ -187,7 +187,16 @@ function SceneManager(canvas) {
         renderer.render(scene, camera);
 
         player.update(elapsedTime, sensorY);
-        
+
+        //update ui health
+        ui.health.text("Health : " + player.body.health);
+        //update ui score
+        ui.score.text("Score : " + gm.score);
+
+        if (player.body.health == 0) {
+            time = false;
+            $('#game-over-container').show();
+        }
 
         stats.end();
 
@@ -208,8 +217,8 @@ function SceneManager(canvas) {
         renderer.setSize(width, height);
     }
 
-    function buildPhysicsContactMaterial(){
-        
+    function buildPhysicsContactMaterial() {
+
         var ground_ground_cm = new CANNON.ContactMaterial(groundMaterial, groundMaterial, {
             friction: 0.4,
             restitution: 0.3,
@@ -227,8 +236,31 @@ function SceneManager(canvas) {
             contactEquationStiffness: 1e8,
             contactEquationRelaxation: 3
         });
-  
+
         world.addContactMaterial(slippery_ground_cm);
     }
 
+    this.clear = function clearBuffer() {
+        // clear program that still running when to change scene or something
+        sound.stop();
+    }
+
 }
+
+
+// const skybox = new Skybox(scene);
+// var text = new MyText(scene);
+
+
+
+// var axesHelper = new THREE.AxesHelper(500);
+// scene.add( axesHelper );
+
+// var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+
+// var rendererStats	= new THREEx.RendererStats()
+// rendererStats.domElement.style.position	= 'absolute'
+// rendererStats.domElement.style.left	= '0px'
+// rendererStats.domElement.style.bottom	= '0px'
+// document.body.appendChild( rendererStats.domElement )
