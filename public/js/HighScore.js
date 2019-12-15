231a38e52a6807d397faa2399fcec22ea7263a0b
Hisam WP <36986744+hisamwp@users.noreply.github.com>
2019-12-15 17:45:34 +0700

    edit
    edit

--- public/js/HighScore.js
+++
@@ -1,7 +0,0 @@
-var skor = new GameUI().lastScore;
-var skortinggi = new highscore(skor);
-//document.write("highscore: " + text(skor))
-
-document.getElementsByClassName("skor-tinggi");
-
-$(".skor-tinggi").appendTo("Highscore: " + text(skortinggi.highscore));
--- public/js/Objects/GameManager.js
+++ public/js/Objects/GameManager.js
@@ -1,10 +1,3 @@
 function GameManager(){
     this.score = 0;
 }
-
-function highscore(score){
-    highscore = 0;
-    if(highscore<score){
-        this.highscore = score;
-    }
-}
